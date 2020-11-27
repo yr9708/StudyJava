1ade86d5372f584f2b7b361bb4f1de610f2ba816
Kang YoeRim <yr9708@hanmail.net>
2020-11-27 23:55:14 +0900

    소수찾기

---
+++ Programmers/Solution17.java
@@ -0,0 +1,42 @@
+package Programmers1;
+
+
+/*
+ * 소수찾기
+ */
+public class Solution17 {
+	public static void main(String[] args) {
+		System.out.println(solution(10));
+	}
+	
+	public static int solution(int n) {
+        int answer = 0;
+
+        boolean[] sosu = new boolean[n+1];
+        for(int i=2; i<=n; i++) { // 2~n번째수를 true로 초기화
+        	sosu[i] = true;
+        }
+        
+        // 제곱근 구하기
+        int root = (int)Math.sqrt(n);
+        
+        
+        for(int i=2; i<=root; i++) { // 2~루트n까지 검사
+        	if(sosu[i]==true) { // i번째의 수가 소수일 때
+        		for(int j=i; i*j<=n; j++) { // 그 배수들을 다 false로 초기화 (배수는 소수가 아니기 때문)
+        			sosu[i*j]=false;
+        		}
+        	}
+        }
+        
+        
+        for(int i=2; i<=n; i++) {
+        	if(sosu[i]==true) {
+        		answer++;
+        	}
+        }
+        
+        
+        return answer;
+    }
+}
